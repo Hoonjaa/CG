262608322de00372053b47cdad5fd2f756fe57d9
Hoonjaa <gnswp8978@gmail.com>
2025-09-16 21:51:29 +0900

    [25.09.16] Update. base code

--- computer_graphics/base.cpp
+++ computer_graphics/base.cpp
@@ -7,12 +7,6 @@
 
 GLvoid drawScene(GLvoid);
 GLvoid Reshape(int w, int h);
-GLvoid Keyboard(unsigned char key, int x, int y);
-
-std::random_device rd;
-std::uniform_int_distribution<float> distribution(0.0f, 1.0f);
-
-GLclampf r = 0.0f, g = 0.0f, b = 0.0f;
 
 void main(int argc, char** argv)
 {
@@ -35,13 +29,12 @@ void main(int argc, char** argv)
 
 	glutDisplayFunc(drawScene);										//출력 함수의 지정
 	glutReshapeFunc(Reshape);										//다시 그리기 함수의 지정
-	glutKeyboardFunc(Keyboard);
 	glutMainLoop();													//이벤트 처리 시작
 }
 
 GLvoid drawScene()													//--- 콜백 함수 : 그리기 콜백 함수
 {
-	glClearColor(r, g, b, 1.0f);									//바탕색을 'blue'로 지정
+	glClearColor(0.0f, 0.0f, 1.0f, 1.0f);									//바탕색을 'blue'로 지정
 	glClear(GL_COLOR_BUFFER_BIT);									//설정된 색으로 전체를 칠하기
 	//그리기 부분 구현
 	//--- 그리기 관련 부분이 여기에 포함된다.
@@ -51,33 +44,4 @@ GLvoid drawScene()													//--- 콜백 함수 : 그리기 콜백 함수
 GLvoid Reshape(int w, int h)
 {
 	glViewport(0, 0, w, h);
-}
-
-GLvoid Keyboard(unsigned char key, int x, int y)
-{
-	switch (key) {
-	case 'c':
-		r = 0.0f; g = 1.0f; b = 1.0f;
-		break;
-	case 'm':
-		r = 1.0f; g = 0.0f; b = 1.0f;
-		break;
-	case 'y':
-		r = 1.0f; g = 1.0f; b = 0.0f;
-		break;
-	case 'a':
-		r = distribution(rd); g = distribution(rd); b = distribution(rd);
-		break;
-	case 'w':
-		break;
-	case 'k':
-		break;
-	case 't':
-		break;
-	case 's':
-		break;
-	case 'q':
-		break;
-	}
-	glutPostRedisplay();
 }
