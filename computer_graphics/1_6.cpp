dba77de916fcc7f44dadab270c9f330f49d7558d
Hoonjaa <gnswp8978@gmail.com>
2025-09-27 17:18:38 +0900

    [25.09.27] Update 1-6 // Rect 클래스 생성

--- computer_graphics/1_6.cpp
+++ computer_graphics/1_6.cpp
@@ -16,6 +16,24 @@ std::random_device rd;
 std::uniform_real_distribution<float> distribution_color(0.0f, 1.0f);
 std::uniform_real_distribution<float> distribution_coordinate(-0.9f, 0.9f);
 
+class Rect {
+	GLclampf r, g, b;
+	GLfloat center_x, center_y;
+	GLfloat size_w, size_h;
+
+	bool dragging = false;
+	GLfloat offset_x = 0.0f, offset_y = 0.0f;
+public:
+	Rect(GLfloat x, GLfloat y);
+	Rect(GLfloat x, GLfloat y, GLfloat w, GLfloat h);
+	//그리기 함수
+	GLvoid draw_rect();
+	GLvoid change_color();
+	//마우스
+	//마우스가 안에 있는지 체크
+	bool mouse_check_in_rect(GLfloat x, GLfloat y);
+};
+
 int WindowWidth = 500, WindowHeight = 500;
 
 void main(int argc, char** argv)
@@ -46,7 +64,7 @@ void main(int argc, char** argv)
 
 GLvoid drawScene()													//--- 콜백 함수 : 그리기 콜백 함수
 {
-	glClearColor(0.0f, 0.0f, 1.0f, 1.0f);									//바탕색을 'blue'로 지정
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);									//설정된 색으로 전체를 칠하기
 	//그리기 부분 구현
 	//--- 그리기 관련 부분이 여기에 포함된다.
@@ -79,4 +97,21 @@ GLvoid Keyboard(unsigned char key, int x, int y)
 GLvoid Win_to_GL_mouse(int x, int y, GLfloat& gl_x, GLfloat& gl_y) {
 	gl_x = (x / (float)WindowWidth) * 2.0f - 1.0f;
 	gl_y = 1.0f - (y / (float)WindowHeight) * 2.0f;
+}
+
+Rect::Rect(GLfloat x, GLfloat y) : r(distribution_color(rd)), g(distribution_color(rd)), b(distribution_color(rd)), center_x(x), center_y(y), size_w(0.1f), size_h(0.1f) {}
+Rect::Rect(GLfloat x, GLfloat y, GLfloat w, GLfloat h) : r(distribution_color(rd)), g(distribution_color(rd)), b(distribution_color(rd)), center_x(x), center_y(y), size_w(w), size_h(h) {}
+
+GLvoid Rect::draw_rect() {
+	glColor3f(r, g, b);
+	glRectf(center_x - size_w, center_y - size_h, center_x + size_w, center_y + size_h);
+}
+
+GLvoid Rect::change_color() {
+	r = distribution_color(rd); g = distribution_color(rd); b = distribution_color(rd);
+}
+
+bool Rect::mouse_check_in_rect(GLfloat x, GLfloat y) {
+	return (x >= center_x - size_w && x <= center_x + size_w &&
+		y >= center_y - size_h && y <= center_y + size_h);
 }
