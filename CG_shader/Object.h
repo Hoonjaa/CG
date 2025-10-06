440801ab00b4abb615c31a4abfc192b5caf4e5ae
Hoonjaa <gnswp8978@gmail.com>
2025-10-06 21:36:35 +0900

    [Update / 25.10.06] 객체 부모 클래스 수정

--- CG_shader/Object.h
+++ CG_shader/Object.h
@@ -1,13 +1,26 @@
 #include "pch.h"
 
 class Object {
-private:
-	GLfloat pos_x, pos_y;
-	GLfloat r, g, b;
-	GLuint VAO, VBO;
+protected:
+	GLfloat pos_x = 0.0f, pos_y = 0.0f;
+	GLfloat r = 1.0f, g = 1.0f, b = 1.0f;
+	GLuint VAO = 0;
+	GLuint VBO = 0;
+	GLint vertexCount = 0;
+
+protected:
+	// 버퍼 생성 및 크기 할당 (데이터는 비움)
+	void allocate(GLsizeiptr size, GLenum usage = GL_STATIC_DRAW);
+	// 데이터 업로드 (부분 갱신 가능)
+	void upload(const void* data, GLsizeiptr size, GLintptr offset = 0);
+	// 속성 설정
+	void setVertexAttrib(GLuint index, GLint size, GLenum type,
+		GLsizei stride, const void* pointer, GLboolean normalized = GL_FALSE);
 
 public:
-	virtual GLvoid init() = 0;				//모양, 색, 위치 초기화
+	Object();
+	virtual ~Object();
+
 	virtual GLvoid draw() = 0;				//그리기
 	virtual GLvoid update() = 0;			//위치 변경
 };
