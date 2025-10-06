a234c246c63a193513258dfa2fa2b4834f684a3c
Hoonjaa <gnswp8978@gmail.com>
2025-10-06 19:44:21 +0900

    [Update / 25.10.06] 기본 헤더 파일 수정

--- CG_shader/pch.h
+++ CG_shader/pch.h
@@ -1,1 +1,15 @@
 #pragma once
+
+#define _CRT_SECURE_NO_WARNINGS											//--- 프로그램 맨 앞에 선언할 것
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+
+#include <gl/glew.h>
+#include <gl/freeglut.h>
+#include <gl/freeglut_ext.h>
+
+struct vec2 {
+	GLfloat x, y;
+	vec2(GLfloat x = 0, GLfloat y = 0) : x(x), y(y) {}
+};
