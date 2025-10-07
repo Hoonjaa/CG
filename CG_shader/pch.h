23c136180e8a72bc6bffd4af3b0abdfb83487bf5
Hoonjaa <gnswp8978@gmail.com>
2025-10-08 00:53:57 +0900

    [Update / 25.10.08] practice_10 // vec3 구조체에 정규화 함수 추가

--- CG_shader/pch.h
+++ CG_shader/pch.h
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <random>
 #include <vector>
+#include <math.h>
 
 #include <gl/glew.h>
 #include <gl/freeglut.h>
@@ -13,7 +14,22 @@
 
 struct vec3 {
 	GLfloat x, y, z;
+public:
 	vec3(GLfloat x = 0, GLfloat y = 0, GLfloat z = 0) : x(x), y(y), z(z) {}
+
+	GLfloat Length() {
+		return sqrt(x * x + y * y + z * z);
+	}
+
+	vec3& Normalize() {
+		GLfloat length = Length();
+		if (length != 0) {
+			x /= length;
+			y /= length;
+			z /= length;
+		}
+		return *this;
+	}
 };
 
 enum class DRAWMODE {
@@ -21,5 +37,14 @@ enum class DRAWMODE {
 	LINE = GL_LINES,
 	TRIANGLE = GL_TRIANGLES,
 	LINE_OBJECT = GL_LINE_LOOP,
+	END
+};
+
+enum class MOVE_TYPE {
+	STOP,
+	BOUNCING,
+	ZIGZAG_HORIZONTAL,
+	SPIRAL_SQUARE,
+	SPIRAL_CIRCLE,
 	END
 };
