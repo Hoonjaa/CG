227b2a95fc80e880a73fc9b9ee0328c980db21ec
Hoonjaa <gnswp8978@gmail.com>
2025-10-11 17:39:01 +0900

    [Update / 25.10.11] 기본 셰이더 파일 변경

--- CG_shader/vertex.glsl
+++ CG_shader/vertex.glsl
@@ -1,12 +1,11 @@
 #version 330 core
 
+layout(location = 0) in vec3 loc;
+layout(location = 1) in vec3 col;
+out vec3 frag_col;
+
 void main()
 {
-	const vec4 vertex[4] = vec4[4](
-		vec4(-0.25, -0.25, 0.5, 1.0),
-		vec4(0.25, -0.25, 0.5, 1.0),
-		vec4(0.0, 0.25, 0.5, 1.0),
-		vec4(0.0, -0.5, 0.5, 1.0)
-	);
-	gl_Position = vertex[gl_VertexID];
+	gl_Position =  vec4(loc, 1.0f);
+	frag_col = col;
 }
