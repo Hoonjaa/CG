bf94fa0e10030c6a65ed957e11ff86be1d629cd1
Hoonjaa <gnswp8978@gmail.com>
2025-10-14 22:40:13 +0900

    [Update / 25.10.14] practice_12 // 선 -> 삼각형 애니메이션 추가

--- CG_shader/TPentagon.cpp
+++ CG_shader/TPentagon.cpp
@@ -9,6 +9,11 @@ TPentagon::TPentagon(GLfloat x, GLfloat y, GLint mode)
 	ExtraVertex[1] = vec3(vPos.x - 0.14f, vPos.y - 0.3f, 0.0f);
 	ExtraVertex[2] = vec3(vPos.x + 0.14f, vPos.y - 0.3f, 0.0f);
 	ExtraVertex[3] = vec3(vPos.x + 0.2f, vPos.y - 0.1f, 0.0f);
+	setVertex[0] = ExtraVertex[0];
+	setVertex[1] = ExtraVertex[1];
+	setVertex[2] = ExtraVertex[2];
+	setVertex[3] = ExtraVertex[3];
+	setVertex[4] = vPos;
 	vColor = vec3(random_color(rd), random_color(rd), random_color(rd));
 	DrawMode = mode;
 
@@ -49,6 +54,10 @@ GLvoid TPentagon::update()
 {
 	if (ShapeType == 0) {
 		TransPentagon();
+	}
+
+	if (ShapeType == 1) {
+		TransLine();
 	}
 
 	GLfloat pentagonVertices[] = {
@@ -112,5 +121,47 @@ GLvoid TPentagon::TransPentagon()
 	if (finish == 5) {
 		ShapeType = 1;
 		DrawMode = (GLint)DRAWMODE::LINE;
+	}
+}
+
+GLvoid TPentagon::TransLine()
+{
+	if (DrawMode != (GLint)DRAWMODE::TRIANGLE)
+		DrawMode = (GLint)DRAWMODE::TRIANGLE;
+	GLint finish = 0;
+
+	//v 이동
+	if (vPos.x > setVertex[4].x) {
+		vPos.x -= 0.01f;
+	}
+	else {
+		vPos.x = setVertex[4].x;
+		finish++;
+	}
+	if (vPos.y < setVertex[4].y) {
+		vPos.y += 0.01f;
+	}
+	else {
+		vPos.y = setVertex[4].y;
+		finish++;
+	}
+	//3 이동
+	if (ExtraVertex[3].x > setVertex[2].x) {
+		ExtraVertex[3].x -= 0.01f;
+	}
+	else {
+		ExtraVertex[3].x = setVertex[2].x;
+		finish++;
+	}
+	if (ExtraVertex[3].y > setVertex[2].y) {
+		ExtraVertex[3].y -= 0.01f;
+	}
+	else {
+		ExtraVertex[3].y = setVertex[2].y;
+		finish++;
+	}
+
+	if (finish == 4) {
+		ShapeType = 2;
 	}
 }
--- CG_shader/TPentagon.h
+++ CG_shader/TPentagon.h
@@ -3,10 +3,12 @@
 class TPentagon : public Object
 {
 private:
+	vec3 setVertex[5];
 	vec3 ExtraVertex[4];
 	GLint ShapeType = 0; //0:오각형, 1:선, 2:삼각형, 3:사각형
 
 	GLvoid TransPentagon();
+	GLvoid TransLine();
 public:
 	TPentagon(GLfloat x, GLfloat y, GLint mode);
 	~TPentagon();
