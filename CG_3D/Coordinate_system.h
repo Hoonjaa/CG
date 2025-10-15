1cd216994aa22cee2defa6c2c8bac29e902cb19f
Hoonjaa <gnswp8978@gmail.com>
2025-10-15 23:34:56 +0900

    [Add / 25.10.15] 기울어진 3차원 좌표계 생성

---
+++ CG_3D/Coordinate_system.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Object.h"
+class Coordinate_system : public Object
+{
+	public:
+	Coordinate_system();
+	virtual ~Coordinate_system();
+	virtual GLvoid draw() override;
+	virtual GLvoid update() override;
+};
+
--- CG_3D/vertex.glsl
+++ CG_3D/vertex.glsl
@@ -4,8 +4,10 @@ layout(location = 0) in vec3 loc;
 layout(location = 1) in vec3 col;
 out vec3 frag_col;
 
+uniform mat4 modelTransform;
+
 void main()
 {
-	gl_Position =  vec4(loc, 1.0f);
+	gl_Position = modelTransform * vec4(loc, 1.0f);
 	frag_col = col;
 }
